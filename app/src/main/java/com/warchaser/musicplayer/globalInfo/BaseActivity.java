ace54eac0b1d5f22aa5f184d47fca8b07a46a6d7
wu.shb <wu.shb@neusoft.com>
2017-08-11 15:53:08 +0800

    fix # bug check URI permission only on SplashActivity start. 	change gradle complie version to 3.0.0-beta1 temporarily. Signed-off-by: wu.shb <wu.shb@neusoft.com>

--- app/src/main/java/com/warchaser/musicplayer/globalInfo/BaseActivity.java
+++ app/src/main/java/com/warchaser/musicplayer/globalInfo/BaseActivity.java
@@ -23,7 +23,7 @@ public class BaseActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkUriPermission();
+//        checkUriPermission();
     }
 
     @Override
